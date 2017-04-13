d61ec9cad3ae9d993ddd204eed1a8f1cc722ebc5
shuq3 <154421897@qq.com>
2017-04-13 17:23:23 +0800

    home页面更新头像接口

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { UserService } from '../../services/user/user.service';
 import { User } from '../../services/user/user';
@@ -12,8 +13,14 @@ export class HomeComponent implements OnInit {
   errorMessage: string = '';
   user: User;
 
-  constructor(public userService: UserService) {
-
+  constructor(public userService: UserService, public router: Router) {
+    userService.getUser().subscribe((data) => {
+      if (data.isOK) {
+        this.user = new User(data.username, data.avatar);
+      } else {
+        router.navigate(['/login', 'sign-in']);
+      }
+    });
   }
 
   ngOnInit() {
