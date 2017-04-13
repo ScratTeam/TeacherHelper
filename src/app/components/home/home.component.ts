480d7008eb27bca6914aad3aa079b91e16ede749
shuq3 <154421897@qq.com>
2017-04-13 16:22:25 +0800

    增加home页面的个人信息

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,13 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../services/user/user';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent implements OnInit {
+  errorMessage: string = '';
+  user: User;
 
-  constructor() { }
+  constructor(public userService: UserService) {
+    this.user = userService.getUser();
+  }
 
   ngOnInit() {
   }
