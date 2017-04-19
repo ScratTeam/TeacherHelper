2f3ef260d7ce7d28ce044b6a5d62a7b074e43494
shuq3 <154421897@qq.com>
2017-04-19 10:48:01 +0800

    更新数据

--- src/app/services/course/course.service.ts
+++ src/app/services/course/course.service.ts
@@ -15,11 +15,33 @@ export class CourseService {
       name: '计算机组成原理',
       classroom: '公教楼 D301',
       time: '每周二 6-8 节课'
+    },{
+      name: '系统分析与设计',
+      classroom: '公教楼 D301',
+      time: '每周三 3-5 节课'
+    },{
+      name: '操作系统',
+      classroom: '公教楼 D301',
+      time: '每周三 3-5 节课'
+    },{
+      name: '编译原理',
+      classroom: '公教楼 D301',
+      time: '每周三 3-5 节课'
+    },{
+      name: '软件测试',
+      classroom: '公教楼 D301',
+      time: '每周三 3-5 节课'
+    },{
+      name: '职业规划与技术管理',
+      classroom: '公教楼 D301',
+      time: '每周三 3-5 节课'
     }];
   }
 
   getCourse(courseName: string) {
-    return this.courses[0];
+    return this.courses.find(function (course) {
+      return course.name == courseName;
+    });
   }
 
   getCourses() {
