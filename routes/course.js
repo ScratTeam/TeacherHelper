c9b2ebf0a8930c29902f915f45bc1570a54c1b08
wangzi <785692920@qq.com>
2017-04-24 17:20:11 +0800

    修改courseSchema数据成员

--- routes/course.js
+++ routes/course.js
@@ -4,39 +4,43 @@ const mongoose = require('mongoose');
 
 // 定义凭证
 const courseSchema = new mongoose.Schema({
+  username: String,
   name: String,
   classroom: String,
-  time: String
+  time: String,
+  // TODO 课堂测验的 MongoDB ID
+  // testIDs: [ObjectId],
+  students: [{
+    id: String,
+    name: String
+  }]
 });
 const Course = mongoose.model('Course', courseSchema);
-
 module.exports = function(app, shareData) {
-  // 创建 router
   var router = new Router({prefix: '/course'});
 
   router.post('/update-course', async function(ctx, next) {
     console.log('course updating(?)');
     // TODO 更新课程信息
-    var reqCourse = ctx.req.body.course;
     // 在实现新增功能之前测试用的
     let course = new Course({
-        name: reqCourse.name,
-        classroom: reqCourse.classroom,
-        time: reqCourse.time
+        name: ctx.req.body.course.name,
+        classroom: ctx.req.body.course.classroom,
+        time: ctx.req.body.course.time
       });
     courses = await Course.find({ name: ctx.req.body.oldName });
     if(courses.length == 0) {
       await course.save();
       ctx.body = { isOk: true,
-        name: reqCourse.name,
-        classroom: reqCourse.classroom,
-        time: reqCourse.time
+        name: ctx.req.body.course.name,
+        classroom: ctx.req.body.course.classroom,
+        time: ctx.req.body.course.time
       };
     } else if(courses.length == 1) {
       var conditions = { _id: courses[0]._id };
       var update = { $set: {
-        classroom: reqCourse.classroom,
-        time: reqCourse.time
+        classroom: ctx.req.body.course.classroom,
+        time: ctx.req.body.course.time
       } };
       var options = { upsert: true};
       Course.update(conditions, update, options, function(error) {
@@ -47,9 +51,9 @@ module.exports = function(app, shareData) {
         }
       });
       ctx.body = { isOk: true,
-        name: reqCourse.name,
-        classroom: reqCourse.classroom,
-        time: reqCourse.time
+        name: ctx.req.body.course.name,
+        classroom: ctx.req.body.course.classroom,
+        time: ctx.req.body.course.time
       };
     } else {
       console.log('something error');
