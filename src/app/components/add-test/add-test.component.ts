bce326e24a97c95cba5b1d40c9a4eca1591c999a
刘忍 <18826073587@163.com>
2017-04-30 11:31:12 +0800

    修改 toolbar，完成添加试题页面的权限校验和布局

--- src/app/components/add-test/add-test.component.ts
+++ src/app/components/add-test/add-test.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { UserService } from '../../services/user/user.service';
+import { User } from '../../services/user/user';
 
 @Component({
   selector: 'app-add-test',
@@ -6,8 +10,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-test.component.sass']
 })
 export class AddTestComponent implements OnInit {
+  user: User;  // 当前登录用户，用于身份校验和页面跳转
 
-  constructor() {}
+  constructor(private userService: UserService, private router: Router) {
+    // 如果用户未登录，则跳转到注册登录页面
+    userService.getUser().subscribe((data) => {
+      if (data.isOK)
+        this.user = new User(data.username, data.avatar,
+                             data.university, data.school);
+      else
+        router.navigate(['/login', 'sign-in']);
+    });
+  }
 
   ngOnInit() {}
 
