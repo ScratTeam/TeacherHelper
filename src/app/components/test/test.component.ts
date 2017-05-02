0e29321d34952ac4a646bef08bb5ffc6c467c551
shuq3 <154421897@qq.com>
2017-05-02 15:35:35 +0800

    完善答题分析的数据显示

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -17,18 +17,26 @@ export class TestComponent implements OnInit {
   test: Test;
   options: any;
   questions: Question[];
-  choiceHide : Boolean[] = [];
-  length: number;
+  // 是否显示分析结果
+  analyseHide : Boolean[] = [];
+  // 答题人数
+  answersNumber: number[] = [];
+  // 该测试是否已经过期
+  valid: Boolean;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute,
               private snackBar: MdSnackBar, private testService: TestService) {
     activatedRoute.params.subscribe((params: Params) => {
       // 取回测试信息
       this.test = this.testService.getTest(params['test']);
+      // 判断是否过期
+      this.valid = new Date() > this.test.endTime;
       this.questions = this.test.questions;
+      // 将分析结果初始化为隐藏
       var i;
       for (i = 0; i < this.questions.length; i++) {
-        this.choiceHide.push(true);
+        this.analyseHide.push(true);
+        this.answersNumber.push(this.questions[i].answers.length);
       }
     });
   }
@@ -41,57 +49,60 @@ export class TestComponent implements OnInit {
   }
 
   expandMore(index) {
-    this.choiceHide[index] = false;
-    var myChart = Highcharts.chart(String(index), {
-    chart: {
-       plotBackgroundColor: null,
-       plotBorderWidth: null,
-       plotShadow: false,
-       type: 'pie'
-    },
-    title: {
-       text: '第'+String(index+1)+'题答题情况'
-    },
-    tooltip: {
-       pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
-    },
-    plotOptions: {
-       pie: {
-           allowPointSelect: true,
-           cursor: 'pointer',
-           dataLabels: {
-               enabled: true,
-               format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-               style: {
-                   color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-               }
-           }
-       }
-    },
-    series: [{
-       name: 'Brands',
-       colorByPoint: true,
-       data: [{
-           name: 'A',
-           y: 56.33
-       }, {
-           name: 'B',
-           y: 24.03,
-           sliced: true,
-           selected: true
-       }, {
-           name: 'C',
-           y: 10.38
-       }, {
-           name: 'D',
-           y: 5.88
+    this.analyseHide[index] = false;
+    // 如果是选择题，则用饼状图显示每个选项的答题人数
+    if (this.questions[index].type == 1) {
+      var myChart = Highcharts.chart(String(index), {
+      chart: {
+         plotBackgroundColor: null,
+         plotBorderWidth: null,
+         plotShadow: false,
+         type: 'pie'
+      },
+      title: {
+         text: '第'+String(index+1)+'题答题情况'
+      },
+      tooltip: {
+         pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+      },
+      plotOptions: {
+         pie: {
+             allowPointSelect: true,
+             cursor: 'pointer',
+             dataLabels: {
+                 enabled: true,
+                 format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+                 style: {
+                     color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+                 }
+             }
+         }
+      },
+      series: [{
+         name: 'Brands',
+         colorByPoint: true,
+         data: [{
+             name: 'A',
+             y: 56.33
+         }, {
+             name: 'B',
+             y: 24.03,
+             sliced: true,
+             selected: true
+         }, {
+             name: 'C',
+             y: 10.38
+         }, {
+             name: 'D',
+             y: 5.88
+         }]
        }]
-     }]
-    });
+      });
+    }
   }
 
   expandLess(index) {
-    this.choiceHide[index] = true;
+    this.analyseHide[index] = true;
   }
 
 }
