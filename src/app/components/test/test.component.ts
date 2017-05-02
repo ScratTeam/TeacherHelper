caf7dc4fabe833f3ad6ccdd8b3420a595c64e493
shuq3 <154421897@qq.com>
2017-05-02 14:16:35 +0800

    显示试题分析的图表

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -17,8 +17,8 @@ export class TestComponent implements OnInit {
   test: Test;
   options: any;
   questions: Question[];
-  choiceHide : Boolean = true;
-  analyseHide : Boolean = true;
+  choiceHide : Boolean[] = [];
+  length: number;
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute,
               private snackBar: MdSnackBar, private testService: TestService) {
@@ -26,6 +26,10 @@ export class TestComponent implements OnInit {
       // 取回测试信息
       this.test = this.testService.getTest(params['test']);
       this.questions = this.test.questions;
+      var i;
+      for (i = 0; i < this.questions.length; i++) {
+        this.choiceHide.push(true);
+      }
     });
   }
 
@@ -36,12 +40,58 @@ export class TestComponent implements OnInit {
            date.getDate();
   }
 
-  expandMore() {
-    this.choiceHide = false;
+  expandMore(index) {
+    this.choiceHide[index] = false;
+    var myChart = Highcharts.chart(String(index), {
+    chart: {
+       plotBackgroundColor: null,
+       plotBorderWidth: null,
+       plotShadow: false,
+       type: 'pie'
+    },
+    title: {
+       text: '第'+String(index+1)+'题答题情况'
+    },
+    tooltip: {
+       pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+    },
+    plotOptions: {
+       pie: {
+           allowPointSelect: true,
+           cursor: 'pointer',
+           dataLabels: {
+               enabled: true,
+               format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+               style: {
+                   color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+               }
+           }
+       }
+    },
+    series: [{
+       name: 'Brands',
+       colorByPoint: true,
+       data: [{
+           name: 'A',
+           y: 56.33
+       }, {
+           name: 'B',
+           y: 24.03,
+           sliced: true,
+           selected: true
+       }, {
+           name: 'C',
+           y: 10.38
+       }, {
+           name: 'D',
+           y: 5.88
+       }]
+     }]
+    });
   }
 
-  expandLess() {
-    this.choiceHide = true;
+  expandLess(index) {
+    this.choiceHide[index] = true;
   }
 
 }
