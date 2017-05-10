6c5b207715b92a59f59c68d6f5d943b28a6e309f
shuq3 <154421897@qq.com>
2017-05-10 09:54:23 +0800

    可以做题的页面

--- src/app/components/test/test.component.ts
+++ src/app/components/test/test.component.ts
@@ -30,7 +30,8 @@ export class TestComponent implements OnInit {
       // 取回测试信息
       this.test = this.testService.getTest(params['test']);
       // 判断是否过期
-      this.valid = new Date() > this.test.endTime;
+      // this.valid = new Date() > this.test.endTime;
+      this.valid = true;
       this.questions = this.test.questions;
       // 将分析结果初始化为隐藏
       var i;
