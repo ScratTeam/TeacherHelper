ec635ee5027522395637f6abdf4b677f7cc61ef9
wangzi <785692920@qq.com>
2017-05-13 09:51:34 +0800

    后端获取测试信息

--- routes/test.js
+++ routes/test.js
@@ -4,6 +4,8 @@ const mongoose = require('mongoose');
 
 // 定义凭证
 testSchema = {
+  username: String,
+  courseName: String,
 	name: String,
 	startTime: Date,
   endTime: Date,
@@ -11,7 +13,7 @@ testSchema = {
 	questions: [{
     type: Number,
     stem: String,
-    choices: String[],
+    choices: [String],
     answers: [{
     	id: String,
     	answer: String
@@ -19,26 +21,98 @@ testSchema = {
     correctStudents: [String],
 	}]
 };
-
-const Test = mongoose.module('Test', testSchema);
+const Test = mongoose.model('Test', testSchema);
 
 module.exports = function(app, shareData) {
   // 创建 router
   var router = new Router({ prefix: '/test' });
 
-  // TODO 后端校验
-  courseValidator = function(ctx) {
-
+  // 后端校验
+  testValidator = function(ctx) {
+    // request body 为空
+    if (ctx.request.body == null || ctx.request.body == undefined) {
+      return false;
+    }
+    // 其他校验
+    else {
+      let courseName = ctx.request.body.courseName;
+      let name = ctx.request.body.name;
+      let startTime = ctx.request.body.startTime;
+      let endTime = ctx.request.body.endTime;
+      let questions = ctx.request.body.questions;
+      // 非法请求
+      if (courseName == null || courseName == undefined ||
+        name == null || name == undefined ||
+        startTime == null || startTime == undefined ||
+        endTime == null || endTime == undefined ||
+        questions == null || questions == undefined ||
+        endTime < startTime) {
+        return false;
+      } else {
+        return true;
+      }
+    }
   }
 
-	// TODO 获取所有测试
-  router.post('get-all-tests', async function(ctx, next) {
-
+	// 获取测试
+  router.post('get-tests', async function(ctx, next) {
+    try {
+      // 若请求不包含用户名，则未授权
+      if (ctx.session.username == null || ctx.session.username == undefined) {
+        ctx.body = { isOk: false, message: '401' };
+      } else if (ctx.request.body == null || ctx.request.body == undefined ||
+        ctx.request.body.courseName == null || ctx.request.body.courseName == undefined) {
+        ctx.status = 403;
+      } else {
+        let tests = await Test.find({
+          username: ctx.session.username,
+          courseName: ctx.request.courseName 
+        });
+        ctx.body = { isOk: true, tests:[] };
+        tests.foreach(function(test) {
+          ctx.body.tests.push({
+            name: test.name,
+            detail: test.detail,
+            startTime: test.startTime,
+            endTime: test.endTime,
+            questions: test.questions
+          });
+        });
+      }
+    } catch(error) {
+      console.log(error);
+    }
   });
 
-  // TODO 获取某个测试
+  // 获取单个测试
   router.post('get-test', async function(ctx, next) {
-
+    try {
+      // 若请求不包含用户名，则未授权
+      if (ctx.session.username == null || ctx.session.username == undefined) {
+        ctx.body = { isOk: false, message: '401' };
+      } else if (ctx.request.body == null || ctx.request.body == undefined ||
+        ctx.request.body.courseName == null || ctx.request.body.courseName == undefined ||
+        ctx.request.body.name == null || ctx.request.body.name == undefined) {
+        ctx.status = 403;
+      } else {
+        let tests = await Test.find({ 
+          username: ctx.session.username,
+          courseName: ctx.request.body.courseName,
+          name: ctx.request.body.name });
+        let test = tests[0];
+        ctx.body = {
+          isOk: true,
+          name: test.name,
+          startTime: test.startTime,
+          endTime: test.endTime,
+          detail: test.detail,
+          questions: test.questions
+        }
+      }
+    } catch(error) {
+      console.log(error);
+    }
   });
+
 
 }
