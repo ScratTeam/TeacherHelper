a024cad15f65a9eb94aa512b874dd693aad3065e
刘忍 <18826073587@163.com>
2017-06-18 19:47:24 +0800

    更改脚本

--- run-server.sh
+++ run-server.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# 进入服务器的 screen
-screen -r server
-
 # 拷贝证书文件
 cd pack
 mkdir ssl
-cp /etc/letsencrypt/live/scrat.pw/fullchain.pem ssl/
-cp /etc/letsencrypt/live/scrat.pw/privkey.pem ssl/
+sudo cp /etc/letsencrypt/live/scrat.pw/fullchain.pem ssl/
+sudo cp /etc/letsencrypt/live/scrat.pw/privkey.pem ssl/
 
 # 重启服务器并退出
 sudo pkill -f node
-npm install
-sudo node https-server.js & (sleep 1 && screen -d server)
-exit
+sudo npm install
+sudo node https-server.js & (sleep 1 && exit)
