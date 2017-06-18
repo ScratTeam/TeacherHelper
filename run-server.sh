2aeefc70cb55538152b838d11f356a70b45009ee
刘忍 <18826073587@163.com>
2017-06-18 17:06:54 +0800

    更新脚本和依赖
    可能需要
    npm remove webpack -g
    npm i webpack --save-dev
    并重新安装 npm 包才能运行

--- run-server.sh
+++ run-server.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
-cp -r ssl pack
-sudo pkill -f node
+
+# 进入服务器的 screen
+screen -r server
+
+# 拷贝证书文件
 cd pack
+mkdir ssl
+cp /etc/letsencrypt/live/scrat.pw/fullchain.pem ssl/
+cp /etc/letsencrypt/live/scrat.pw/privkey.pem ssl/
+
+# 重启服务器并退出
+sudo pkill -f node
 npm install
-sudo node https-server.js
+sudo node https-server.js & (sleep 1 && screen -d server)
+exit
